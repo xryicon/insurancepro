a28a99ba0f8dcfba269365d6ebca60057a1c791c
xryicon <xryicon@gmail.com>
2026-05-22 23:41:16 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',  // Change this from '/insurancepro/' to '/'
-  server: {
-    port: 3000,
-    open: true
-  }
 })
