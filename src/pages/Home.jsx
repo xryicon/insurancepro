446e1abdc1a3810c0138c81b6843c5275295e3ea
xryicon <xryicon@gmail.com>
2026-05-23 11:35:15 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,8 +2,8 @@ import { Helmet } from 'react-helmet-async';
 import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
-import Button from '../components/Button';
-import Card from '../components/Card';
+import Button from '../components/ui/Button';
+import Card from '../components/ui/Card';
 
 export default function Home() {
   const { t } = useTranslation();
