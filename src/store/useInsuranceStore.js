a2f72d7af072345454e52c975cc99fb9b0d0e88c
xryicon <xryicon@gmail.com>
2026-05-23 11:22:38 +0200

    Update useInsuranceStore.js

--- src/store/useInsuranceStore.js
+++ src/store/useInsuranceStore.js
@@ -1,1 +1,31 @@
+import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 
+const useInsuranceStore = create(
+  persist(
+    (set) => ({
+      // State
+      carInsuranceData: {},
+      homeInsuranceData: {},
+
+      // Actions
+      setCarInsuranceData: (data) =>
+        set({ carInsuranceData: { ...data } }),
+      setHomeInsuranceData: (data) =>
+        set({ homeInsuranceData: { ...data } }),
+      resetCarInsuranceData: () => set({ carInsuranceData: {} }),
+      resetHomeInsuranceData: () => set({ homeInsuranceData: {} }),
+      resetAll: () =>
+        set({ carInsuranceData: {}, homeInsuranceData: {} }),
+    }),
+    {
+      name: 'insurance-storage', // Key for localStorage
+      partialize: (state) => ({
+        carInsuranceData: state.carInsuranceData,
+        homeInsuranceData: state.homeInsuranceData,
+      }),
+    }
+  )
+);
+
+export default useInsuranceStore;
