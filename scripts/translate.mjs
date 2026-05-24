578fbc62ed98fea1b699f79087e1fcfb97ef98a4
xryicon <xryicon@gmail.com>
2026-05-24 20:28:14 +0200

    Update translate.mjs

--- scripts/translate.mjs
+++ scripts/translate.mjs
@@ -7,7 +7,7 @@ import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__dirname);
+const __dirname = dirname(__filename);
 
 (async () => {
   // 1. Extract all text from components
