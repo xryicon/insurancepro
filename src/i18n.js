563c4bc522f6e3b9590f61fd9bee016bc027e7c9
xryicon <xryicon@gmail.com>
2026-05-24 17:58:16 +0200

    Update i18n.js

--- src/i18n.js
+++ src/i18n.js
@@ -2,10 +2,10 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
-// Import translation files
-import enTranslations from './public/locales/en/translation.json';
-import esTranslations from './public/locales/es/translation.json';
-import nlTranslations from './public/locales/nl/translation.json';
+// Import translation files using dynamic imports (for Vite compatibility)
+const enTranslations = await import('./public/locales/en/translation.json').then(m => m.default);
+const esTranslations = await import('./public/locales/es/translation.json').then(m => m.default);
+const nlTranslations = await import('./public/locales/nl/translation.json').then(m => m.default);
 
 i18n
   .use(LanguageDetector)
